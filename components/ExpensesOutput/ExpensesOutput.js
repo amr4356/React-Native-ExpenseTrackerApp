6a87dc52c4fdcaf1d762c9f18ea88ae04cd1949a
amr4356 <amrooo73321@gmail.com>
2024-10-11 17:47:35 +0300

    143. Managing App-wide State With Context

--- components/ExpensesOutput/ExpensesOutput.js
+++ components/ExpensesOutput/ExpensesOutput.js
@@ -3,62 +3,7 @@ import ExpensesSummary from "./ExpensesSummary";
 import ExpensesList from "./ExpensesList";
 import { GlobalStyles } from "../../constants/styles";
 
-const DUMMY_EXPENSES = [
-  {
-    id: "e1",
-    description: "A pair of shoes",
-    amount: 59.99,
-    date: new Date("2021-12-19"),
-  },
-  {
-    id: "e2",
-    description: "A pair of trousers",
-    amount: 89.99,
-    date: new Date("2022-01-05"),
-  },
-  {
-    id: "e3",
-    description: "some apples",
-    amount: 5.99,
-    date: new Date("2021-12-01"),
-  },
-  {
-    id: "e4",
-    description: "A book",
-    amount: 30.99,
-    date: new Date("2020-01-01"),
-  },
-  {
-    id: "e5",
-    description: "Another book",
-    amount: 18.59,
-    date: new Date("2022-04-23"),
-  },
-  {
-    id: "e6",
-    description: "A pair of trousers",
-    amount: 89.99,
-    date: new Date("2022-01-05"),
-  },
-  {
-    id: "e7",
-    description: "some apples",
-    amount: 5.99,
-    date: new Date("2021-12-01"),
-  },
-  {
-    id: "e8",
-    description: "A book",
-    amount: 30.99,
-    date: new Date("2020-01-01"),
-  },
-  {
-    id: "e9",
-    description: "Another book",
-    amount: 18.5923,
-    date: new Date("2022-04-23"),
-  },
-];
+
 
 function ExpensesOutput({ expenses, expensesPeriod }) {
   return (
