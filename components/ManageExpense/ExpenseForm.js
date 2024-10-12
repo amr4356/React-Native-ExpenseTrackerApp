40b99709a140f41c0ef32cc183b62b3ab7cafc29
amr4356 <amrooo73321@gmail.com>
2024-10-12 23:09:34 +0300

    149. Creating an Overall Form

--- components/ManageExpense/ExpenseForm.js
+++ components/ManageExpense/ExpenseForm.js
@@ -1,9 +1,29 @@
 import { StyleSheet, View } from "react-native";
+import Input from "./Input";
 
 export default function ExpenseForm() {
-  return <View>
-    
-  </View>;
+  function amountChangedHandler() {}
+
+  return (
+    <View>
+      <Input
+        label="Amount"
+        textInputConfig={{
+          keyboardType: "decimal-pad",
+          onChangeText: amountChangedHandler,
+        }}
+      />
+      <Input
+        label="Date"
+        textInputConfig={{
+          placeholder: "YYYY-MM-DD",
+          maxLength: 10,
+          onChangeText: () => {},
+        }}
+      />
+      <Input label="Description" textInputConfig={{}} />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({});
--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -1,9 +1,10 @@
 import { useContext, useLayoutEffect } from "react";
-import { StyleSheet, TextInput, View } from "react-native";
+import { StyleSheet, View } from "react-native";
 import IconButton from "../components/UI/IconButton";
 import { GlobalStyles } from "../constants/styles";
 import Button from "../components/UI/Button";
 import { ExpensesContext } from "../context/expenses-context";
+import ExpenseForm from "../components/ManageExpense/ExpenseForm";
 
 function ManageExpense({ route, navigation }) {
   const { deleteExpense, updateExpense, addExpense } =
@@ -47,7 +48,7 @@ function ManageExpense({ route, navigation }) {
 
   return (
     <View style={styles.container}>
-      <TextInput />
+      <ExpenseForm />
       <View style={styles.buttons}>
         <Button style={styles.button} mode="flat" onPress={cancelHandler}>
           Cancel
