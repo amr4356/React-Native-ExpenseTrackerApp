726a2ed5bd0ad107f09f1d681c6fb923048e7fa5
amr4356 <amrooo73321@gmail.com>
2024-10-14 15:39:50 +0300

    169. Managing the Loading State

--- screens/RecentExpenses.js
+++ screens/RecentExpenses.js
@@ -1,22 +1,30 @@
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import ExpensesOutput from "../components/ExpensesOutput/ExpensesOutput";
 import { ExpensesContext } from "../context/expenses-context";
 import { getDateMinusDays } from "../util/date";
 import { fetchExpenses } from "../util/http";
+import LoadingOverlay from "../components/UI/LoadingOverlay";
 
 function RecentExpenses() {
-  const { expenses ,setExpenses} = useContext(ExpensesContext);
+  const { expenses, setExpenses } = useContext(ExpensesContext);
 
+  const [isFetching, setIsFetching] = useState(true);
 
   useEffect(() => {
     async function getExpenses() {
-      const expenses=await fetchExpenses();
+      setIsFetching(true);
+      const expenses = await fetchExpenses();
+      setIsFetching(false);
       setExpenses(expenses);
     }
 
     getExpenses();
   }, []);
 
+  if (isFetching) {
+    return <LoadingOverlay />;
+  }
+  
   const recentExpenses = expenses.filter((expense) => {
     const today = new Date();
     const date7DaysAgo = getDateMinusDays(today, 7);
